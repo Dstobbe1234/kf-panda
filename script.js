7f875e311b4e38070b9516c01699895adb466cf7
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-14 20:06:14 -0700

    Done

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 // KungFu Panda Character Search
+//Variables for HTML Elements
+let searchBtnEl = document.getElementById("search");
+let charImgEl = document.getElementById("char-img");
+let nameInputEl = document.getElementById("name-input");
+let charNameEl = document.getElementById("char-name");
+let charQuoteEl = document.getElementById("char-quote");
 // Add event listener
-document.getElementById("search").addEventListener("click", characterSearch);
+searchBtnEl.addEventListener("click", characterSearch);
 
 // Event Function
 function characterSearch() {
 
 // Get Input
-let character = document.getElementById("name-input").value;
+let character = nameInputEl.value.toLowerCase();
 
 // Check the name
-if(character === "Po") {
-    document.getElementById("char-img").src = "images/po.png";
-    document.getElementById("char-name").innerHTML = "Po";
-    document.getElementById("char-quote").innerHTML = "Buddy, I am the Dragon Warrior";
-} else if (character === "Viper") {
-    document.getElementById("char-img").src = "images/viper.png";
-    document.getElementById("char-name").innerHTML = "Viper";
-    document.getElementById("char-quote").innerHTML = "I don't need to bite to fight!";
-} else if (character === "Mantis") {
-    document.getElementById("char-img").src = "images/mantis.png";
-    document.getElementById("char-name").innerHTML = "Mantis";
-    document.getElementById("char-quote").innerHTML = "Fear the bug!";
-} else if (character === "Crane") {
-    document.getElementById("char-img").src = "images/crane.png";
-    document.getElementById("char-name").innerHTML = "Crane";
-    document.getElementById("char-quote").innerHTML = "You can chain my body, but you will never chain my warrior spirit!";
-} else if (character === "Tigress") {
-    document.getElementById("char-img").src = "images/tigress.png";
-    document.getElementById("char-name").innerHTML = "Tigress";
-    document.getElementById("char-quote").innerHTML = "That was pretty hardcore!";
+if(character === "po" || character === "the dragon warrior" || character === "dragon warrior") {
+    charImgEl.src = "images/po.png";
+    charNameEl.innerHTML = "Po";
+    charQuoteEl.innerHTML = "Buddy, I am the Dragon Warrior!";
+} else if (character === "viper") {
+    charImgEl.src = "images/viper.png";
+    charNameEl.innerHTML = "Viper";
+    charQuoteEl.innerHTML = "I don't need to bite to fight!";
+} else if (character === "mantis") {
+    charImgEl.src = "images/mantis.png";
+    charNameEl.innerHTML = "Mantis";
+    charQuoteEl.innerHTML = "Fear the bug!";
+} else if (character === "crane") {
+    charImgEl.src = "images/crane.png";
+    charNameEl.innerHTML = "Crane";
+    charQuoteEl.innerHTML = "You can chain my body, but you will never chain my warrior spirit!";
+} else if (character === "tigress") {
+    charImgEl.src = "images/tigress.png";
+    charNameEl.innerHTML = "Tigress";
+    charQuoteEl.innerHTML = "That was pretty hardcore!";
 } else {
-    document.getElementById("char-img").src = "images/question-mark.png";
-    document.getElementById("char-name").innerHTML = "-------";
-    document.getElementById("char-quote").innerHTML = "Character Not Found.";
+    charImgEl.src = "images/question-mark.png";
+    charNameEl.innerHTML = "-------";
+    charQuoteEl.innerHTML = "Character Not Found.";
 }
 }
